4f6d9a66ac2abe5cb4bdf1c8d8e912cda17e9553
ykocar <77811733+ykocar@users.noreply.github.com>
2026-08-20 17:34:04 +0100

    Bump http-verbs-test-play-30 to 15.8.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"            %% "api-test-runner"         % "0.9.0",
-    "uk.gov.hmrc"            %% "http-verbs-test-play-30" % "15.7.0",
+    "uk.gov.hmrc"            %% "http-verbs-test-play-30" % "15.8.0",
     "org.scalatestplus.play" %% "scalatestplus-play"      % "7.0.2",
     "org.playframework"      %% "play-pekko-http-server"  % "3.0.10"
   ).map(_ % Test)
